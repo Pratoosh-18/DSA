667b80740bc7e0db219400f3e32a447ebe9f53a5
Pratoosh-18 <pratoosh10garg@gmail.com>
2024-09-21 23:07:31 +0530

    Length of last word in string

---
+++ String/LengthOfLastWord.java
@@ -0,0 +1,12 @@
+package String;
+
+public class LengthOfLastWord {
+    public static int lolw(String s){
+        String[] words = s.split(" ");
+        return words[words.length-1].length();
+    }
+    public static void main(String[] args) {
+        int len = lolw("Your string here");
+        System.out.println(len);
+    }
+}
