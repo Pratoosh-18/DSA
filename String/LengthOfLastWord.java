1e94fafb92b506d1f09407072d518b9076470ea2
Pratoosh Garg <pratooshgarg@Pratooshs-MacBook-Air.local>
2024-10-01 17:01:04 +0530

    Check

--- String/LengthOfLastWord.java
+++ String/LengthOfLastWord.java
@@ -4,7 +4,7 @@ public class LengthOfLastWord {
     public static int lolw(String s){
         String[] words = s.split(" ");
         return words[words.length-1].length();
-    }
+    } 
     public static void main(String[] args) {
         int len = lolw("Your string here");
         System.out.println(len);
